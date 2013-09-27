6592e73dc788c998093a013ca8a3b1634f22897c
Paul Balomiri <paulbalomiri@gmail.com>
2013-09-27 11:00:52 +0300

    modified maivic-comm-protocol to be able to commit

--- libs/maivic-comm-protocol/src/test/java/net/maivic/comm/testdata/LocalTestDataGenerator.java
+++ libs/maivic-comm-protocol/src/test/java/net/maivic/comm/testdata/LocalTestDataGenerator.java
@@ -1,48 +1,43 @@
 package net.maivic.comm.testdata;
 
 
+import java.util.List;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import net.maivic.protobuf.Maivic.Address;
-import net.maivic.protobuf.Maivic.Location;
-import net.maivic.protobuf.Maivic.VersionedID;
+import net.maivic.protocol.Model.Address;
+import net.maivic.protocol.Model.Location;
 
 public class LocalTestDataGenerator {
-	Map<VersionedID, Address> addresses_by_version_id = new HashMap<VersionedID, Address>();
+	List<Address> addresses_by_version_id = new ArrayList< Address>();
 	public LocalTestDataGenerator() {
 		for (Address.Builder a: addresses){
-			addresses_by_version_id.put(a.getId(), a.build());
+			addresses_by_version_id.add( a.build());
 		}
 	}
-	public  Address getAddress(VersionedID ver) {
-		return addresses_by_version_id.get(ver);
+	public  Address getAddress() {
+		return addresses_by_version_id.get(addresses_by_version_id.size()-1);
 				
 	}
 	
-	private static VersionedID.Builder id(long id, long ver){
-		return VersionedID.newBuilder().setId(id).setVersion(ver);
-	}
-	private static Address.Builder genAddress(VersionedID.Builder ver, String city, String street, String nr, String zip, String country, String county, double lat, double lon) {
+	private static Address.Builder genAddress(int id,String city, String street, String nr, String zip, String country, String county, double lat, double lon) {
 		return Address.newBuilder()
-				.setId(ver)
+				.setId(id)
 				.setCity(city)
 				.setStreet(street)
 				.setNumber(nr)
-				.setZipcode(zip)
 				.setCountry(country)
 				.setCounty(county)
-				.setLocation(Location.newBuilder()
-								.setLat(lat)
-								.setLon(lon));	
+								;	
 
 	}
 	private static final Address.Builder[] addresses = {
-		genAddress(id(0,0),"$Bucharest$","Calea Serban Voda","66","102123","$Romania$","$Bucharest$",44.406368,26.095635),
-		genAddress(id(1,0),"$Bucharest$","Piata Charles de Gaulle","5A","101123","$Romania$","$Bucharest$",44.406368,26.095635),
-		genAddress(id(2,0),"$Bucharest$","Piata Charles de Gaulle","1","101123","$Romania$","$Bucharest$",44.406368,26.095635),
-		genAddress(id(3,0),"$Bucharest$","Alea Alexandru","9","101123","$Romania$","$Bucharest$",44.406368,26.095635),
-		genAddress(id(4,0),"$Bucharest$","Popa Savu","75","101123","$Romania$","$Bucharest$",44.406368,26.095635),
+		genAddress(0,"$Bucharest$","Calea Serban Voda","66","102123","$Romania$","$Bucharest$",44.406368,26.095635),
+		genAddress(1,"$Bucharest$","Piata Charles de Gaulle","5A","101123","$Romania$","$Bucharest$",44.406368,26.095635),
+		genAddress(2,"$Bucharest$","Piata Charles de Gaulle","1","101123","$Romania$","$Bucharest$",44.406368,26.095635),
+		genAddress(3,"$Bucharest$","Alea Alexandru","9","101123","$Romania$","$Bucharest$",44.406368,26.095635),
+		genAddress(4,"$Bucharest$","Popa Savu","75","101123","$Romania$","$Bucharest$",44.406368,26.095635),
 		
 	};
 	
--- libs/maivic-comm-protocol/src/test/java/net/maivic/comm/tests/TestNotificationDispatch.java
+++ libs/maivic-comm-protocol/src/test/java/net/maivic/comm/tests/TestNotificationDispatch.java
@@ -1,15 +1,8 @@
 package net.maivic.comm.tests;
 
 import net.maivic.comm.CallBackDispatcherThread;
-import net.maivic.comm.Callback;
 import net.maivic.comm.Factory;
-import net.maivic.comm.TestConfig;
-import net.maivic.comm.TestFactory;
 import net.maivic.comm.TransportManager;
-import net.maivic.protobuf.Maivic.UserNotification;
-import net.maivic.protobuf.Maivic.UserNotification.NotificationType;
-import net.maivic.protobuf.Maivic.UserNotification.Priority;
-import net.maivic.protobuf.Message.MaivicMessage;
 
 import org.junit.After;
 import org.junit.Before;
@@ -21,27 +14,7 @@ public class TestNotificationDispatch {
 	private CallBackDispatcherThread cb_thread;
 	@Before 
 	public void setUp(){
-		factory=new TestFactory();
-		this.transportManager=factory.createTransportManager();
-		this.cb_thread= new CallBackDispatcherThread();
-		for(int x = 0; x<10; ++x) {
-			cb_thread.insertCb(new Callback<UserNotification>(){
-	
-				public void call(UserNotification result) {
-				 //TODO: Implement!!!
-					
-				}
-	
-				public void onError(Throwable t) {
-					
-					
-				}} , 
-			
-				UserNotification.newBuilder()
-					.setType(NotificationType.valueOf(x%NotificationType.values().length))
-					.setMessage("This is info # "+ x +" for the User").build()
-				, x*1000);
-		}
+		
 	}
 	
 	@After
