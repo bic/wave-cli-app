0b2aa9fa4575374de9aa3bcb653c7986a1469499
Paul Balomiri <paulbalomiri@gmail.com>
2013-10-07 16:25:16 +0300

    added docstrings for the Utils class

--- apps/android/wave-cli-android-lib/src/com/maivic/android/Utils.java
+++ apps/android/wave-cli-android-lib/src/com/maivic/android/Utils.java
@@ -1,15 +1,37 @@
 package com.maivic.android;
 
 import java.math.BigDecimal;
-
+/**
+ * Class aggregating helper functions for the numberpicker
+ * @author paul
+ *
+ */
 public class Utils {
 	private static BigDecimal zero = new BigDecimal(0); 
-	
+	/**
+	 * calculates the next value using the current value as reference
+	 * @See {@link Utils.next(BigDecimal value, BigDecimal step, BigDecimal reference, BigDecimal min, BigDecimal max)} 
+	 */
 	
 	public static BigDecimal nextWithoutReference(BigDecimal value, BigDecimal step, BigDecimal min, BigDecimal max) {
 		return next(value, step, value, min, max);
 	}
 	
+	/**
+	 * Function calculating the next numberpicker value given the current value as in the {@code value} parameter.
+	 * The function has special behaivors around limits:
+	 * if value = min or max then:
+	 *  next value is rounded to the next value where (reference -newvalue) % step == 0
+	 * if value+step is >max or < min the next step is max or min respectively 
+	 * 
+	 * @param value the current value
+	 * @param step the step for calculating the next value
+	 * @param reference the reference value for stepping
+	 * @param min minimum value
+	 * @param max maximum value
+	 * @return the next value
+	 * @throws IllegalArgumentException for step==0
+	 */
 	public static BigDecimal next(BigDecimal value, BigDecimal step, BigDecimal reference, BigDecimal min, BigDecimal max) {
 	
 		BigDecimal limit = null;	
